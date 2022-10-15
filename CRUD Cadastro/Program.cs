bc64da34806c4b6e86bc8649df8b1e6ad715bdda
Donizetti Oliveira <44945021+Donztt@users.noreply.github.com>
2022-10-15 04:43:45 -0300

    teste

---
+++ CRUD Cadastro/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CRUD_Cadastro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
